bb15942cb5f1dae0eda939b884f233d6e72a8417
ronnievm <ronnievm20@outlook.com>
2018-06-23 14:46:03 +0200

    Created command to show the server advertisement whitelist

---
+++ cmds/serverwhitelist.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    let whitelist = await bot.db.total('SELECT * FROM serverwhitelist');
+    let serverListEmbed = await new Discord.RichEmbed()
+    .setColor(message.guild.member(message.author).highestRole.color || "#FF0000")
+    .setTimestamp();
+
+    let serverList = "\n";
+    for(i = 0; i < whitelist.length; ++i) {
+        serverList += `${whitelist[i].id}\n`;
+    }
+    
+    userListEmbed.addField(`Users [${whitelist.length}]`, `\`\`\`${serverList}\`\`\``);
+    message.channel.send(serverListEmbed);
+}
+
+module.exports.help = {
+    name: "cbwhitelist",
+    description: "Returns a list of users with chainban permissions.",
+    type: "help",
+    usage: "`prefix (cbwhitelist | cbwl)`"
+}
+
+module.exports.conf = {
+    aliases: ["cbwl"]
+}
